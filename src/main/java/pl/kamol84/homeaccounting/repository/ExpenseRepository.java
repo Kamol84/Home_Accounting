17ac2e1cf28d3bdbe1bd02737a0f9f14646dd92a
Kamol84 <kamilwrz@gmail.com>
2019-03-28 23:12:45 +0100

    Added few user reports.

--- src/main/java/pl/kamol84/homeaccounting/repository/ExpenseRepository.java
+++ src/main/java/pl/kamol84/homeaccounting/repository/ExpenseRepository.java
@@ -1,9 +1,21 @@
 package pl.kamol84.homeaccounting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.kamol84.homeaccounting.entity.Expense;
+import pl.kamol84.homeaccounting.entity.User;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+    List<Expense> findAllByUser(User user);
 
+    @Query("SELECT expenses from Expense expenses WHERE user_id = :user_id and year(created)= :year and month(created)= :month")
+    List<Expense> userExpenseFromYearMonth(@Param("user_id") Long user_id, @Param("year") Integer year, @Param("month") Integer month);
+
+    @Query("SELECT SUM(expenses.value) from Expense expenses WHERE user_id = :user_id and year(created)= :year and month(created)= :month")
+    BigDecimal sumUserExpenseFromMonth(@Param("user_id") Long user_id, @Param("year") Integer year, @Param("month") Integer month);
 }
