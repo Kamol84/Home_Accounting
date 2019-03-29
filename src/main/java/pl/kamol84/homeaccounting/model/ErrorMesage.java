0d316800e6c603f5953b7eaa74f4851d76605cba
Kamol84 <kamilwrz@gmail.com>
2019-03-29 11:54:25 +0100

    error view fix.

---
+++ src/main/java/pl/kamol84/homeaccounting/model/ErrorMesage.java
@@ -0,0 +1,58 @@
+package pl.kamol84.homeaccounting.model;
+
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class ErrorMesage {
+
+    private LocalDateTime timestamp;
+    private String status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorMesage() {
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- src/main/java/pl/kamol84/homeaccounting/service/ServiceErrorAdvice.java
+++ src/main/java/pl/kamol84/homeaccounting/service/ServiceErrorAdvice.java
@@ -1,29 +1,52 @@
 package pl.kamol84.homeaccounting.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.kamol84.homeaccounting.model.ErrorMesage;
 
 import javax.persistence.EntityNotFoundException;
+import java.net.URI;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ServiceErrorAdvice {
 
+    @Autowired
+    ErrorMesage errorMesage;
+
     @ExceptionHandler({DataIntegrityViolationException.class})
     public ResponseEntity handle(DataIntegrityViolationException e) {
-        return new ResponseEntity("Dane nie mogą się powtarzać", null, HttpStatus.BAD_REQUEST);
+        errorMesage.setTimestamp(LocalDateTime.now());
+        errorMesage.setStatus("400");
+        errorMesage.setError(HttpStatus.BAD_REQUEST.name());
+        errorMesage.setMessage( "Dane nie mogą się powtarzać");
+//        errorMesage.setPath();
+
+        return new ResponseEntity(errorMesage, null, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({EntityNotFoundException.class})
     public ResponseEntity handle(EntityNotFoundException e) {
-        return new ResponseEntity("wybrana grupa nie istnieje", null, HttpStatus.BAD_REQUEST);
+        errorMesage.setTimestamp(LocalDateTime.now());
+        errorMesage.setStatus("404");
+        errorMesage.setError(HttpStatus.NOT_FOUND.name());
+        errorMesage.setMessage("wybrana grupa nie istnieje");
+//        errorMesage.setPath();
+        return new ResponseEntity(errorMesage, null, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({IllegalStateException.class})
     public ResponseEntity handle(IllegalStateException e) {
-        return new ResponseEntity("niekompletne dane", null, HttpStatus.BAD_REQUEST);
+        errorMesage.setTimestamp(LocalDateTime.now());
+        errorMesage.setStatus("400");
+        errorMesage.setError(HttpStatus.BAD_REQUEST.name());
+        errorMesage.setMessage("niekompletne dane");
+        //        errorMesage.setPath();
+        return new ResponseEntity(errorMesage, null, HttpStatus.BAD_REQUEST);
     }
 
 }
